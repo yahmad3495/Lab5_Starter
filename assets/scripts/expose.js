456b9f4378b63773d131faa5f162cac267061e2a
Younus Ahmad <69169095+yahmad3495@users.noreply.github.com>
2022-10-28 14:55:57 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,18 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let drop_down = document.getElementById("horn-select");
+  let image = document.getElementById("horn_pic");
+  drop_down.addEventListener('input', function() {
+    let horn_type = drop_down.value;
+    if (horn_type == "air-horn") {
+      image.src = "assets/images/air-horn.svg";
+    }
+    else if (horn_type == "car-horn") {
+      image.src = "assets/images/car-horn.svg";
+    }
+    else if (horn_type == "party-horn") {
+      image.src = "assets/images/party-horn.svg";
+    }
+  });
 }
